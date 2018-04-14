2a2c89a71ca0573cf7cc5cfbefb2c3f842c11161
sahil.gu <sahilkkrazy@gmail.com>
2018-04-15 00:13:55 +0530

    added 2 MF

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const server = express();
-const proxy = require('http-proxy-middleware');
+const request = require('request');
 
 server.set('view engine', 'ejs');
 
-server.get('/', (req, res) => res.render('index'));
+server.get('/', (req, res) =>
+  Promise.all([
+  	getContents('http://localhost:4000/'),
+    getContents('http://localhost:7777/'),
+  ]).then(responses =>{
+  	res.render('index', { graphControl: responses[0], graphDashboard: responses[1] })
+  }).catch(error =>
+    res.send(error.message)
+  )
+);
 
-server.use('*', proxy({
-  target: 'https://microfrontends.herokuapp.com',
-  changeOrigin: true
-}));
+const getContents = (url) => new Promise((resolve, reject) => {
+  request.get(url, (error, response, body) => {
+    if (error) return resolve("Error loading " + url + ": " + error.message);
 
-server.use(express.static('build'));
+    return resolve(body);
+  });
+});
 
-const port = process.env.PORT || 9999;
+const port = process.env.PORT || 9000;
 server.listen(port, () => {
   console.log(`Homepage listening on port ${port}`);
 });
