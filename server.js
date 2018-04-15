320cd496472765c47a646a26f31d3155a014778a
Sahil Gupta <sahilkkrazy@gmail.com>
2018-04-15 18:47:54 +0530

    bundle structure added

--- server.js
+++ server.js
@@ -1,52 +1,30 @@
 const express = require('express');
 const server = express();
 const request = require('request');
+const fs = require('fs');
+const path = require('path');
 
-var DOM={
-	js : [],
-	css : []
+var DOM={js : [],css : []};
+var app = {graphControl : 'http://localhost:4000/',graphDashboard : 'http://localhost:7777/'
 };
+
+
 server.set('view engine', 'ejs');
+server.use(express.static(__dirname + '/public'));
 
-server.get('/', (req, res) =>
-  Promise.all([
-  	getContents('http://localhost:4000/'),
-    getContents('http://localhost:7777/'),
-  ]).then(responses =>{
-  	responses[0] = processDOM(responses[0],'http://localhost:4000/');
-  	responses[1] = processDOM(responses[1],'http://localhost:7777/');
+server.get('/', function(req, res) {
+	fs.readFile(path.resolve(__dirname, "./public/app.html"), (err, data) => {
+	  	if (err) throw err;
+		res.render('index', {appHTML : data });  	
+	})
+});
 
-  	var script = "";  	
-  	var style = "";   
+const port = process.env.PORT || 9000;
+server.listen(port, () => {
+  console.log(`Homepage listening on port ${port}`);
+});
 
-  	var promise =  new Promise(function(resolve, reject) {
-  		Promise.all(
-	  		DOM.js.map(function(obj){
-		  		return getContents(obj.source+obj.data)
-		  	})).then(responses =>{
-	  			responses.forEach(function(resp){
-	  				script += '<script type="text/javascript">'+resp+'</script>';
-	  			})
-	  			Promise.all(
-			  		DOM.css.map(function(obj){
-				  		return getContents(obj.source+obj.data)
-				  	})).then(responses =>{
-			  			responses.forEach(function(resp){
-			  				style +=resp;
-			  			})
-			  			resolve();
-			  		})	
-	  		})	
-  	})
-  	
-  	promise.then(function(response){
-  		res.render('index', {includeScripts: script, includeStyle : style, graphControl: responses[0], graphDashboard: responses[1] });
-  	});
-  }).catch(error =>
-    res.send(error.message)
-  )
-);
-
+// Helper Functions
 const getContents = (url) => new Promise((resolve, reject) => {
   request.get(url, (error, response, body) => {
     if (error) return resolve("Error loading " + url + ": " + error.message);
@@ -95,10 +73,61 @@ const processDOM = (html,source) => {
 	return html
 }
 
+function init(){
+	Promise.all([
+	  	getContents(app.graphControl),
+	    getContents(app.graphDashboard)
+	  ]).then(responses => {
+	  		var script = "";  	
+		  	var style = "";
+
+		  	responses[0] = processDOM(responses[0],app.graphControl);
+		  	responses[1] = processDOM(responses[1],app.graphDashboard);
+
+		  	fs.writeFile('public/app.html', responses[0].concat(responses[1]) , (err) => {
+			  if (err) throw err;
+			});
+
+			var promise =  new Promise(function(resolve, reject) {
+		  		Promise.all(
+			  		DOM.js.map(function(obj){
+				  		return getContents(obj.source+obj.data)
+				  	})).then(responses =>{
+			  			responses.forEach(function(resp){
+			  				script += resp;
+			  			})
+			  			fs.writeFile('public/js-bundle.js', script , (err) => {
+						  if (err) throw err;
+						  console.log('The JS bundle is created');
+						});
+			  			Promise.all(
+					  		DOM.css.map(function(obj){
+						  		return getContents(obj.source+obj.data)
+						  	})).then(responses =>{
+					  			responses.forEach(function(resp){
+					  				style +=resp;
+					  			})
+					  			fs.writeFile('public/css-bundle.css', style , (err) => {
+								  if (err) throw err;
+								  console.log('The CSS bundle is created');
+								});
+					  			resolve();
+					  		})	
+			  		})
+		  	})
+
+		  	promise.then(function(response){
+				console.log("App is ready to serve");
+			});
+	  })
+}
+
+// Get style source
 function getStyleSrc(str){
 	return str.split("href=")[1].split(".css")[0].slice(1);
 }
 
+// Get script source
 function getAttributeValue(attribute, source)
 {
     var regex = RegExp("<script?\\w+(?:\\s+(?:" + attribute + "=\"([^\"]*)\")|[^\\s>]+|\\s+)*>","gi");
@@ -110,8 +139,4 @@ function getAttributeValue(attribute, source)
     return a
 }
 
-
-const port = process.env.PORT || 9000;
-server.listen(port, () => {
-  console.log(`Homepage listening on port ${port}`);
-});
+init();
